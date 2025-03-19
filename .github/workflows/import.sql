c5b014bf6f9ddfac7e140212fde5155aae213084
Michèle BARRE <michele.barre@opt.nc>
2025-03-20 10:53:08 +1100

    ci: check data integrity

---
+++ .github/workflows/import.sql
@@ -0,0 +1,31 @@
+CREATE OR REPLACE TABLE mapping_gh_kaggle (
+    gh_handle VARCHAR NOT NULL UNIQUE,
+    kaggle_handle VARCHAR,-- NOT NULL UNIQUE,
+
+    --PRIMARY KEY (gh_handle, kaggle_handle)
+);
+
+insert into mapping_gh_kaggle
+select *
+from '../../data/relationship_gh_kaggle.csv';
+
+
+CREATE OR REPLACE TABLE feedback_sessions (
+    session_date DATE NOT NULL,
+    id_etudiant_gh varchar NOT NULL,
+    duree_heures integer NOT NULL DEFAULT 3,
+    niko_niko varchar NOT NULL check (niko_niko in ('VERT', 'JAUNE', 'ROUGE')),
+    fdbck_clarte_explications integer CHECK (fdbck_clarte_explications BETWEEN 1 AND 5),
+    fdbck_utilite_concepts integer CHECK (fdbck_utilite_concepts BETWEEN 1 AND 5),
+    fdbck_difficulte_percue integer CHECK (fdbck_difficulte_percue BETWEEN 1 AND 5),
+    fdbck_suggestions_amelioration varchar(50),
+    fdbck_impact_progression integer CHECK (fdbck_impact_progression BETWEEN 1 AND 5),
+    fdbck_confiance_atteinte_objectif_professionnel integer CHECK (fdbck_confiance_atteinte_objectif_professionnel BETWEEN 1 AND 5),
+    fdbck_ce_que_j_ai_prefere varchar(50),
+    fdbck_commentaires_supplementaires varchar(50),
+    PRIMARY KEY (session_date, id_etudiant_gh)
+);
+
+insert into feedback_sessions
+select *
+from '../../data/feedback_sessions.csv';
